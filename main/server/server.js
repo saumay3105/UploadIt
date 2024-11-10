c3be18722bc024d1e6dabc47e02d2809f960ea48
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-11-10 23:57:46 +0530

    Update server.js

--- main/server/server.js
+++ main/server/server.js
@@ -84,7 +84,7 @@ app.delete('/api/delete-file/:fileName', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
